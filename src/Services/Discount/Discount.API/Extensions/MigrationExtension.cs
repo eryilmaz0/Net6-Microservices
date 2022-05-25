7388ddf6517b218866346fb360df54a59fc87999
Eren YILMAZ <eryilmaz0@hotmail.com>
2022-05-25 21:13:42 +0300

    Discount API Completed

---
+++ src/Services/Discount/Discount.API/Extensions/MigrationExtension.cs
@@ -0,0 +1,57 @@
+﻿using Npgsql;
+
+namespace Discount.API.Extensions;
+
+public static class MigrationExtension
+{
+    public static IServiceCollection MigrateDatabase<TContext>(this IServiceCollection serviceCollection, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            
+                var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+                var logger = serviceProvider.GetRequiredService<ILogger<TContext>>();
+                try
+                {
+                    logger.LogInformation("Migrating postresql database.");
+
+                    using var connection = new NpgsqlConnection
+                        (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand
+                    {
+                        Connection = connection
+                    };
+
+                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("Migrated postresql database.");
+                }
+                catch (NpgsqlException ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the postresql database");
+
+                    if (retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        System.Threading.Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(serviceCollection, retryForAvailability);
+                    }
+                } 
+            return serviceCollection;
+        }
+}
---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,29 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.MigrateDatabase<Program>();
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
